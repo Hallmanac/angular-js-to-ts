e9aaac5355e1439bdd9ef3c54da75cd723cdfddd
Brian Hall <brian@nebbiatech.com>
2016-04-02 03:42:01 -0400

    Removed unnecessary comments and the like.

--- gulp/typescript.js
+++ gulp/typescript.js
@@ -12,36 +12,34 @@ var tsProject = $.typescript.createProject("tsconfig.json");
 /**
  * Compile TypeScript and include references to library and app .d.ts files.
  */
-gulp.task('compile-ts', function () {
-    var sourceTsFiles = ["src/client/**/*.ts", "missing-typings/**/*.d.ts", "typings/browser/**/*.ts", "typings/browser.d.ts"];//, //path to typescript files
-                         //config.libraryTypeScriptDefinitions]; //reference to library .d.ts files
-
+gulp.task('compile-ts', function() {
+    var sourceTsFiles = ["src/client/**/*.ts",
+        "missing-typings/**/*.d.ts",
+        "typings/browser/**/*.ts",
+        "typings/browser.d.ts"];
 
     var tsResult = gulp.src(sourceTsFiles)
-                       //.pipe($.sourcemaps.init())
-                       .pipe($.typescript(tsProject));
+        .pipe($.typescript(tsProject));
 
-    //tsResult.dts.pipe(gulp.dest(config.tsOutputPath));
     return tsResult.js
-                    //.pipe($.sourcemaps.write('.'))
-                    .pipe(gulp.dest("ts-output/client"));
+        .pipe(gulp.dest("ts-output/client"));
 });
 
 /**
  * Remove all generated JavaScript files from TypeScript compilation.
  */
-gulp.task('clean-ts', function (cb) {
+gulp.task('clean-ts', function(cb) {
     var typeScriptGenFiles = [
-                                'ts-output/**/*.js',    // path to all JS files auto gen'd by editor
-                                'ts-output/**/*.js.map', // path to all sourcemap files auto gen'd by editor
-                                'ts-output/**/*.d.ts',
-                                '!ts-output/lib'
+        'ts-output/**/*.js',    // path to all JS files auto gen'd by editor
+        'ts-output/**/*.js.map', // path to all sourcemap files auto gen'd by editor
+        'ts-output/**/*.d.ts',
+        '!ts-output/lib'
     ];
 
     // delete the files
     $.del(typeScriptGenFiles, cb);
 });
 
-gulp.task('watch-ts', function () {
+gulp.task('watch-ts', function() {
     gulp.watch(["src/**/*.ts"], ['compile-ts']);
 });
